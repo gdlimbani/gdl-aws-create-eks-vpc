5e096010c511ddd3659b555e9bd945d6b7c31b7e
Gautam Limbani <gautamlimbani@yahoo.com>
2025-02-13 13:12:34 +0530

    Extending code to cover as much aws resources as possible

---
+++ gdl-terraform-eks/resource.tf
@@ -0,0 +1,50 @@
+# Define the IAM Policy
+resource "aws_iam_policy" "eks_policy" {
+  name        = "gdl-eks-policy"
+  description = "EKS Policy for creating node groups and related permissions"
+  policy      = jsonencode({
+    Version = "2012-10-17"
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "kms:CreateKey",
+          "kms:DescribeKey",
+          "kms:ListKeys",
+          "kms:TagResource",
+          "kms:CreateAlias",
+          "kms:DeleteAlias",
+          "kms:ListAliases",
+          "kms:PutKeyPolicy"
+        ],
+        "Resource": "*"
+      },
+      {
+        "Effect": "Allow",
+        "Action": [
+          "logs:CreateLogGroup",
+          "logs:TagResource",
+          "logs:DescribeLogGroups",
+          "logs:ListTagsForResource",
+          "logs:PutLogEvents",
+          "logs:DeleteLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutRetentionPolicy"
+        ],
+        "Resource": "*"
+      }
+	  ]
+  })
+}
+
+# Fetch the IAM user to whom the policy will be attached
+resource "aws_iam_user" "eks_user" {
+  name = "gautam.limbani"
+}
+
+# Attach the policy to the IAM user
+resource "aws_iam_policy_attachment" "eks_policy_attachment" {
+  name       = "eks-policy-attachment"
+  users      = [aws_iam_user.eks_user.name]
+  policy_arn = aws_iam_policy.eks_policy.arn
+}
